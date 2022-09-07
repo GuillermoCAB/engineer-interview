526b835da47996208bafee331ab49faf6af99811
GuillermoCAB <guiga.benitez@gmail.com>
2022-09-07 13:04:26 -0300

    Add base types

---
+++ src/types/ChanllengeComponent.ts
@@ -0,0 +1,10 @@
+export declare namespace ChallengeComponentTypes {
+  export interface IColumn {
+    title: string;
+    contents: IContent[];
+  }
+
+  export interface IContent {
+    title: string;
+  }
+}
